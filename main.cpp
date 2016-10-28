f478cd1077b6d18b3ba9f87db46f09b1e33ff60f
patrick03524 <dibujanteanime024@gmail.com>
2016-10-28 13:35:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,23 @@ int main()
     Point a[4];
     a[3]= 2,44;
     PointArray pa1{a,4}
-    pointArray pa2{pa1}
     
-    Point b{14, 25};
-    pA2.pushBack(nuevo1);
+    Point b{23,24};
+    pa1.pushBack(b);
     
-    
-    cout << "Tu array de puntos es: " << endl;
+    Point c{15,16};
+    pa1.insert(1, c);
+    pa1.showPoint(1);
+    pa1.showPoint(4);
+    cout << pa1.getSize() << endl;
+    pa1.remove(3);
+    pa1.showPoint(3);
+    cout << pa1.getSize() << endl;
+    Point *pt = pa1.get(3);   
+    cout << pt->getX() << endl;
+    cout << pt->getY() << endl;
+    pa1.clear();
+    cout << pa1.getSize() << endl;
+
     return 0;
 }
