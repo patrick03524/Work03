52b5ccf2f6e67a6c1c571715e9dbb36e2cd4c525
patrick03524 <dibujanteanime024@gmail.com>
2016-10-28 12:51:13 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "PointArray.h"
+
+using namespace std;
+
+
+int main()
+{
+    Point a[4];
+    PointArray(a,4)
+    cout << "Tu array de puntos es: " << endl;
+    return 0;
+}
