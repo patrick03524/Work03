860bba452a28360365fd0b076d539a89d4ac1b30
patrick03524 <dibujanteanime024@gmail.com>
2016-10-28 13:01:19 -0500

    Create PointArray.h

---
+++ PointArray.h
@@ -0,0 +1,40 @@
+#ifndef POINTARRAY_H_INCLUDED
+#define POINTARRAY_H_INCLUDED
+
+class Point
+{
+private:
+    int x, y;
+public:
+    Point(int x1 = 0, int y1 = 0);
+    Point(Point &o);
+    int getX() const;
+    int getY() const;
+    void setX(const int &x2);
+    void setY(const int &y2);
+
+};
+
+class PointArray
+{
+private:
+    void resize(const int &newLength);
+    Point* firstPoint;
+    int len;
+
+public:
+    PointArray();
+    PointArray(const Point points[], const int &_size);
+    PointArray(const PointArray &pv);
+    ~PointArray();
+    void showPoint(const int &pos);
+    void pushBack(const Point &p);
+    void insert(const int &position, const Point &p);
+    void remove(const int &position);
+    const int getSize() const;
+    void clear();
+    Point* get(const int &position);
+    const Point* get(const int &position) const;
+
+
+#endif // POINTARRAY_H_INCLUDED
