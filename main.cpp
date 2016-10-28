df28708cfcd05f62ba7d1cecf2010874a15ac6cc
patrick03524 <dibujanteanime024@gmail.com>
2016-10-28 13:27:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
-
 int main()
 {
     Point a[4];
-    PointArray(a,4)
+    a[3]= 2,44;
+    PointArray pa1{a,4}
+    pointArray pa2{pa1}
+    
+    Point nuevo1{14, 25};
+    pA2.pushBack(nuevo1);
+    
+    
     cout << "Tu array de puntos es: " << endl;
     return 0;
 }
